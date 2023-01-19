1a3e28925e6348eb992f23c0b635a8da97b6d2b0
kodekingpin <wwitkowski354@gmail.com>
2023-01-19 13:34:57 +0000

    forum started

--- src/components/forum page/forum.js
+++ src/components/forum page/forum.js
@@ -2,8 +2,52 @@ import "./forum.css"
 
 export default function Forum() {
     return (
-        <div className="mainforum">
-            Forum Page
+        <div className="forumback">
+            <div className="mainforum">
+                <div className="topbar">
+                    <div className="postsort">
+                        <h3>Latest</h3>
+                        <h3>Top</h3>
+                    </div>
+                    <input placeholder="Search..." type="search" className="searchbar"></input>
+                    <button>Create Post</button>
+                </div>
+                <div className="bottomarea">
+                    <div className="tagsection">
+                        <div>
+                            <h4>Tags</h4>
+                            <div className="tags">
+                                <label>
+                                    <input type="checkbox"></input>
+                                    TAG
+                                </label>
+                                <label>
+                                    <input type="checkbox"></input>
+                                    TAG
+                                </label>
+                                <label>
+                                    <input type="checkbox"></input>
+                                    TAG
+                                </label>
+                                <label>
+                                    <input type="checkbox"></input>
+                                TAG
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rightside">
+                        <div className="statnames">
+                            <h4>Likes</h4>
+                            <h4>Replies</h4>
+                            <h4>Views</h4>
+                        </div>
+                        <div className="posts">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/posts/smallpost.js
@@ -0,0 +1,19 @@
+import "./smallpost.css"
+
+export default function SmallPost() {
+    return (
+
+        <div>
+            <h2>"Title Post"</h2>
+            <div>
+                <h4>"Author"</h4>
+                <h4>"Date Posted"</h4>
+            </div>
+            <div>
+                <h4>"0"</h4>
+                <h4>"0"</h4>
+                <h4>"0"</h4>
+            </div>
+        </div>
+    )
+}
