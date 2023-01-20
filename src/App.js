9c4975a3a6120f8604885982c9d271bfdbeaad7c
kodekingpin <wwitkowski354@gmail.com>
2023-01-20 13:58:16 +0000

    pref and dash

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/login page/login.js';
 import Home from './components/home page/home.js';
 import NavBar from "./components/nav bar/navbar.js"
 import Forum from './components/forum page/forum';
+import Dashboard from './components/dashboard/dashboard.js';
+import Preferences from './components/preferences/preferences.js';
 
-
+//https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications#step-1-building-a-login-page
 
 export default function App() {
   return (
@@ -15,6 +17,8 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/forum' element={<Forum />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/preferences' element={<Preferences/>} />
       </Routes>
     </BrowserRouter>
   );
